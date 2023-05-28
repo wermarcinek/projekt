75bcb3fe274b8aeb1ee7e3046f237f857f69fffb
wermarcinek <72862143+wermarcinek@users.noreply.github.com>
2023-05-28 17:45:26 +0200

    a

---
+++ app/docs/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\App\\Kernel",
+            "name": "Kernel",
+            "summary": "Class\u0020Kernel",
+            "url": "classes/App-Kernel.html"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\App",
+            "name": "App",
+            "summary": "",
+            "url": "namespaces/app.html"
+        }            ]
+);
---
+++ app/public/js/script.js
@@ -0,0 +1,3 @@
+{# ... #}
+<script src="{{ asset('js/script.js') }}"></script>
+{# ... #}
