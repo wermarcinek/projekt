454bb5772095e5c9b9f39ad86b680ab2e2957b7a
wermarcinek <72862143+wermarcinek@users.noreply.github.com>
2023-06-29 18:34:33 +0200

    poprawki pt.2

---
+++ app/src/Form/Type/ChangeEmailType.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Change password type.
+ */
+
+namespace Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+
+/**
+ * Class UserType.
+ */
+class ChangeEmailType extends AbstractType
+{
+    /**
+     * Builds the form.
+     *
+     * This method is called for each type in the hierarchy starting from the
+     * top most type. Type extensions can further modify the form.
+     *
+     * @param FormBuilderInterface $builder The form builder
+     * @param array<string, mixed> $options Form options
+     *
+     * @see FormTypeExtensionInterface::buildForm()
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(
+                'password',
+                PasswordType::class,
+                [
+                    'label' => 'label.currentPassword',
+                    'required' => true,
+                    'constraints' => [
+                        new UserPassword([
+                            'message' => 'Błędne hasło.',
+                        ]), ],
+                ]
+            )
+
+            ->add('newEmail', EmailType::class, [
+                'label' => 'label.newEmail',
+                'required' => true,
+            ]);
+    }
+
+    /**
+     * Configures the options for this type.
+     *
+     * @param OptionsResolver $resolver The resolver for the options
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(['data_class' => null]);
+    }
+
+    /**
+     * Returns the prefix of the template block name for this type.
+     *
+     * The block prefix defaults to the underscored short class name with
+     * the "Type" suffix removed (e.g. "UserProfileType" => "user_profile").
+     *
+     * @return string The prefix of the template block name
+     */
+    public function getBlockPrefix(): string
+    {
+        return 'user';
+    }
+}
